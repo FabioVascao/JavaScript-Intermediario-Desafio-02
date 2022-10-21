c6a47af55037764d4dff8eeceb6371bc522be3a1
Fabio de Sousa Santos <109177231+FabioVascao@users.noreply.github.com>
2022-10-21 13:01:46 -0300

    h1 e cards

--- js/index.js
+++ js/index.js
@@ -26,6 +26,7 @@ const timer = Timer()
 
 const buttonModeLight = document.querySelector('.mode')
 const buttonModeDark = document.querySelector('.mode2')
+const h1 = document.querySelector('h1');
 
 function pressButtonMode() {
   buttonModeLight.classList.toggle('hide');
@@ -36,10 +37,20 @@ buttonModeLight.addEventListener('click', function() {
   pressButtonMode();
   const body = document.querySelector('body');
   body.style.backgroundColor = '#121214';
+  buttonForest.classList.add('dark');
+  buttonRain.classList.add('dark');
+  buttonMarket.classList.add('dark');
+  buttonFire.classList.add('dark');
+  h1.classList.add('dark')
 })
 
 buttonModeDark.addEventListener('click', function() {
   pressButtonMode();
   const body = document.querySelector('body');
   body.style.backgroundColor = '#ffffff';
+  buttonForest.classList.remove('dark');
+  buttonRain.classList.remove('dark');
+  buttonMarket.classList.remove('dark');
+  buttonFire.classList.remove('dark');
+  h1.classList.remove('dark');
 })
