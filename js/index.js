e438c08f1c396aa9422c2594759ffceb267fd783
Fabio de Sousa Santos <109177231+FabioVascao@users.noreply.github.com>
2022-10-21 15:24:10 -0300

    fail volume

--- js/index.js
+++ js/index.js
@@ -5,3 +5,19 @@ import Mode from "./mode.js"
 const sound = Sounds()
 const timer = Timer()
 const mode = Mode()
+
+const buttonPressForest = new Audio("https://github.com/FabioVascao/JavaScript-Intermediario-Desafio-01/blob/main/sounds/Floresta.wav?raw=true")
+let volumeForest = document.querySelector('#volumeForest');
+let x = Number(volumeForest)
+console.log(x)
+
+function volumeInput() { 
+   if( buttonPressForest.volume < 1) {
+     buttonPressForest.volume += 0.1;
+} 
+    if( buttonPressForest.volume > 0)  { buttonPressForest.volume -= 0.1;
+}
+  console.log(volumeForest)
+}
+
+volumeForest.addEventListener("input", volumeInput);
