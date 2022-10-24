035b69d9057846aad1606f798ec71a5fb4a4a741
FabioVascão <fabiosousa2691@hotmail.com>
2022-10-24 19:27:40 -0300

    finalizado

--- js/index.js
+++ js/index.js
@@ -1,18 +1,38 @@
 import Sounds from "./sounds.js"
 import Timer from "./timer.js"
 import Mode from "./mode.js"
+import {
+  buttonPressForest,
+  buttonPressRain,
+  buttonPressMarket,
+  buttonPressFire,
+} from "./elements.js"
 
 const sound = Sounds()
 const timer = Timer()
 const mode = Mode()
 
-const buttonPressForest = new Audio("https://github.com/FabioVascao/JavaScript-Intermediario-Desafio-01/blob/main/sounds/Floresta.wav?raw=true") 
-const forestInput = forest.querySelector('input')
-
-function adjustVolume() {
-  buttonPressForest.volume = forestInput.value
-}
+let forestInput = document.querySelector('#volForest')
+let marketInput = document.querySelector('#volMarket')
+let rainInput = document.querySelector('#volRain')
+let fireInput = document.querySelector('#volFire')
 
 forestInput.addEventListener('input', () => {
-  adjustVolume()
+  let forestVolume = Number(forestInput.value) / 100;
+  buttonPressForest.volume = forestVolume;
+})
+
+marketInput.addEventListener('input', () => {
+  let marketVolume = Number(marketInput.value) / 100;
+  buttonPressMarket.volume = marketVolume;
+})
+
+rainInput.addEventListener('input', () => {
+  let rainVolume = Number(rainInput.value) / 100;
+  buttonPressRain.volume = rainVolume;
+})
+
+fireInput.addEventListener('input', () => {
+  let fireVolume = Number(fireInput.value) / 100;
+  buttonPressFire.volume = fireVolume;
 })
